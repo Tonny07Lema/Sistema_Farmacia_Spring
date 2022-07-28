bf158342e06075bbd2abc40e9ebb49780144cd9e
Tonny07Lema <tonnylema09@gmail.com>
2022-07-27 22:51:30 -0500

    Comentario

--- src/main/java/ups/edu/ec/Sistema_Farmacia_S/SistemaFarmaciaSApplication.java
+++ src/main/java/ups/edu/ec/Sistema_Farmacia_S/SistemaFarmaciaSApplication.java
@@ -2,9 +2,16 @@ package ups.edu.ec.Sistema_Farmacia_S;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SistemaFarmaciaSApplication {
+public class SistemaFarmaciaSApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
+        return builder.sources(SistemaFarmaciaSApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SistemaFarmaciaSApplication.class, args);
